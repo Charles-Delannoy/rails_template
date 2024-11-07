7ba76e8183baf59cc8929ec12186e25f75998d2e
Charles Delannoy <charles.delannoy@hei.fr>
2024-11-07 23:47:18 +0100

    Use a structure.sql file

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Preco
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    # Use a sql db structure file
+    config.active_record.schema_format = :sql
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
