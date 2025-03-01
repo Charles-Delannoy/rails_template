f0c01943ee424fa4a556ff904ae04530e7d2b76a
Charles Delannoy <charles.delannoy@hei.fr>
2025-03-01 09:03:52 +0100

    upd

--- docker_pg.rb
+++ docker_pg.rb
@@ -1,6 +1,7 @@
 run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 
 # Docker for PG
+########################################
 run 'touch docker-compose.yml'
 require 'yaml'
 docker_config = {
@@ -20,14 +21,14 @@ docker_config = {
 }
 File.write("docker-compose.yml", docker_config.to_yaml[0..-4].gsub("5434:5432", '"5434:5432"'))
 
-# database config
+# Database config
+########################################
 yaml_db_config = File.read("config/database.yml")
 partitioned_db_config = yaml_db_config.partition("default: &default\n")
 partitioned_db_config.insert(2,
     "  username: <%= ENV['POSTGRES_USER'] %>\n  password: <%= ENV['POSTGRES_PASSWORD'] %>\n  port: <%= ENV['DATABASE_PORT'] || \"5432\" %>\n  host: <%= ENV['DATABASE_HOST'] || \"127.0.0.1\" %>\n")
 yaml_db_config = partitioned_db_config.join
 File.write("config/database.yml", yaml_db_config)
-
 
 # Gemfile
 ########################################
@@ -41,26 +42,34 @@ inject_into_file "Gemfile", before: "group :development, :test do" do
   RUBY
 end
 
-inject_into_file "Gemfile", before: "group :development, :test do" do
-    <<~RUBY
-      # build front with vite for rails
-      gem 'vite_rails'
-
-    RUBY
-  end
-
-  inject_into_file "Gemfile", after: "group :development, :test do" do
-    "\n  gem \"dotenv-rails\""
-  end
-
-# Use a structure.sql file
-########################################
-inject_into_file "config/application.rb", after: "  class Application < Rails::Application" do
-    <<~RUBY
-        # Use a sql db structure file
-        config.active_record.schema_format = :sql
-    RUBY
+inject_into_file "Gemfile", after: "group :development, :test do" do
+  "\n  gem \"dotenv-rails\""
 end
+
+inject_into_file "Gemfile", before: "group :development, :test do" do
+  <<~RUBY
+    # build front with vite for rails
+    gem 'vite_rails'
+    gem 'dotenv-rails'
+  RUBY
+end
+
+# General Config
+########################################
+general_config = <<~RUBY
+  config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
+RUBY
+
+environment general_config
+
+########################################
+# After bundle
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command "db:drop db:create db:migrate"
+  generate("simple_form:install", "--bootstrap")
 
   # Gitignore
   ########################################
@@ -89,5 +98,5 @@ end
   ########################################
   git :init
   git add: "."
-  git commit: "-m 'Docker PG template from https://raw.githubusercontent.com/Charles-Delannoy/rails_template/main/basic.rb'"
+  git commit: "-m 'Initial commit with minimal template from https://github.com/lewagon/rails-templates'"
 end
